42c8e893f4f3051525ca5ff7423958cd36fa2e7a
jeffrey <eshunjeffrey12@gmail.com>
2025-09-15 00:39:17 +0000

    update

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import pytop
 from itsdangerous import TimestampSigner, BadSignature, SignatureExpired
 
 signer = TimestampSigner(os.environ.get("MAGIC_KEY", "supersecret"))
+
+#MAGIC LINKS TO BE USED
+def create_a_magic_token(em)
