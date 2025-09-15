12f26902c89e6072610d5b1224f57b898aaa43fd
jeffrey <eshunjeffrey12@gmail.com>
2025-09-15 01:06:20 +0000

    update

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from fastapi import FastAPI, HTTPException, Request, Form
 from fastapi.responses import JSONResponse
-from auth_security import create_a_magic_token,
+from auth_security import create_a_magic_token, verify_magic_token,
