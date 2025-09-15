b21a89af627c1515fa6edcd1dfd49ac4bb997578
jeffrey <eshunjeffrey12@gmail.com>
2025-09-15 01:10:31 +0000

    update

--- auth_security.py
+++ auth_security.py
@@ -19,4 +19,4 @@ def verify_magic_token(token: str, max_age=900) -> str | None:
 
 #TOTP's
 def generate_totp_secret() -> str:
-    return base64
+    return base64.b32encode(secrets.token_bytes(10)).decode()
