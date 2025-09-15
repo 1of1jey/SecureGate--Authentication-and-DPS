6cc0a948bd3ef2dd457332ddac819f4fa129e92e
jeffrey <eshunjeffrey12@gmail.com>
2025-09-15 00:47:03 +0000

    update

--- main.py
+++ main.py
@@ -7,3 +7,4 @@ signer = TimestampSigner(os.environ.get("MAGIC_KEY", "supersecret"))
 #MAGIC LINKS TO BE USED
 def create_a_magic_token(email:str) -> str:
     payload = f"{email}:{secrets.token_urlsafe(32)}"
+    return signer
